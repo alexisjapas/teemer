a49a184078821afd999f563c39d05cf37d203d0d
alexisjapas <alexis.japas.dev@pm.me>
2025-08-09 18:54:08 +0200

    fix: more accurate preview/gen factor

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@ pub const PREVIEW_MODE: bool = cfg!(feature = "preview");
 pub const WINDOW_WIDTH: f32 = 720.0;
 pub const WINDOW_HEIGHT: f32 = 1280.0;
 pub const FRAMERATE: f32 = 30.0;
-pub const MAX_DURATION: f32 = 61.0 * if PREVIEW_MODE { 4.0 } else { 1.0 };
+pub const MAX_DURATION: f32 = 61.0 * if PREVIEW_MODE { 3.0 } else { 1.0 };
 pub const MAX_FRAMES_TO_CAPTURE: u32 = MAX_DURATION as u32 * FRAMERATE as u32;
 pub const FIXED_TIME_STEP: f32 = 1.0 / FRAMERATE;
 
@@ -14,7 +14,7 @@ pub const TITLE_FONT_SIZE: f32 = 24.0 * WINDOW_WIDTH / 720.0;
 pub const TEXT_FONT_SIZE: f32 = 20.0 * WINDOW_WIDTH / 720.0;
 
 /// Simulation parameters
-pub const MAX_SPEED: f32 = 21.0 * WINDOW_WIDTH / 720.0 * if PREVIEW_MODE { 4.0 } else { 1.0 };
+pub const MAX_SPEED: f32 = 21.0 * WINDOW_WIDTH / 720.0 * if PREVIEW_MODE { 3.0 } else { 1.0 };
 pub const MOVEMENT_ENERGY_COST_FACTOR: f32 = 3.0E-5;
 pub const ENERGY_TRANSFER_RATE: f32 = 1.0 / 2.0;
 
