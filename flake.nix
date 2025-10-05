7a11baed03ca1f7c7bbac7e458efb1653cf50b3a
alexisjapas <alexis.japas.dev@pm.me>
2025-10-05 22:56:23 +0200

    fix: adapt shell commands to pipe encoder

--- flake.nix
+++ flake.nix
@@ -46,32 +46,10 @@
           cargo run --features bevy/dynamic_linking,preview "$@"
           echo "Preview done."
         '';
-        dev-genframes = pkgs.writeShellScriptBin "dev-genframes" ''
-          echo "Running the frames generation."
-          cargo run "$@"
-          echo "Frames generation done."
-        '';
-        dev-genvid = pkgs.writeShellScriptBin "dev-genvid" ''
-          echo "Targeting latest output frames."
-          # Get the latest folder name matching pattern sim_*Z
-          LATEST_SIM=$(find outputs -maxdepth 1 -type d -name "sim_*Z" | sort | tail -1 | xargs basename)
-          
-          if [ -z "$LATEST_SIM" ]; then
-            echo "No simulation folders found"
-            exit 1
-          fi
-          echo "Rendering video for $LATEST_SIM."
-          ffmpeg -framerate 30 -i outputs/$LATEST_SIM/frames/frame_%06d.png \
-          -c:v h264_nvenc -preset p7 -rc vbr -b:v 8M -maxrate 8M -bufsize 16M \
-          -pix_fmt yuv420p -movflags +faststart \
-          outputs/$LATEST_SIM/$LATEST_SIM.mp4
-
-          echo "Video generation done."
-        '';
         dev-gen = pkgs.writeShellScriptBin "dev-gen" ''
-          dev-genframes
-          sleep 1
-          dev-genvid
+          echo "Running the video generation."
+          cargo run "$@"
+          echo "Video generation done."
         '';
       in
       {
@@ -90,8 +68,6 @@
 
               # Custom commands
               dev-preview
-              dev-genframes
-              dev-genvid
               dev-gen
             ]
             ++ libraries;
@@ -106,9 +82,7 @@
           shellHook = ''
             echo "TeemLabs commands:"
             echo "- \`dev-preview\`: Run a preview."
-            echo "- \`dev-genframes\`: Generate frames."
-            echo "- \`dev-genvid\`: Generate video using the latest simulation's frames."
-            echo "- \`dev-gen\`: Generate frames then video."
+            echo "- \`dev-gen\`: Generate a video."
           '';
         };
       }
